15d4cea4a4b07ffd4893313d62ab87c19442743a
Stepan Beznosov <sbez@mb-901915.local>
2019-03-19 11:31:25 +0300

    new gitignore

--- app/src/androidTest/java/com/sqisland/tutorial/recipes/ui/main/MainActivityTest.java
+++ app/src/androidTest/java/com/sqisland/tutorial/recipes/ui/main/MainActivityTest.java
@@ -14,6 +14,7 @@ import com.sqisland.tutorial.recipes.R;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +25,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import com.microsoft.appcenter.espresso.Factory;
+import com.microsoft.appcenter.espresso.ReportHelper;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -32,13 +35,16 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
     @Test
     public void mainActivityTest() {
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(7000);
+            Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -86,6 +92,11 @@ public class MainActivityTest {
                                 3),
                         isDisplayed()));
         textView4.check(matches(withText("Fried Tomatoes")));
+    }
+
+    @After
+    public void TearDown(){
+        reportHelper.label("Stopping App");
     }
 
     private static Matcher<View> childAtPosition(
